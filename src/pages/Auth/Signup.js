35f30d455657f90f030585fa1d4c6bca8ea5d735
Milan <comiv92@gmail.com>
2020-07-14 13:00:46 +0200

    Updates the signupHandler to use GraphQL

--- src/pages/Auth/Signup.js
+++ src/pages/Auth/Signup.js
@@ -18,7 +18,7 @@ class Signup extends Component {
         value: '',
         valid: false,
         touched: false,
-        validators: [required, length({ min: 5 })]
+        validators: [required, length({ min: 2 })]
       },
       name: {
         value: '',
