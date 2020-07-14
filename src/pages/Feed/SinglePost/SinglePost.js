b4fe92e336bd7b7d5ad9415fd25d8941be529cda
Milan <comiv92@gmail.com>
2020-07-14 16:48:04 +0200

    Updates the singlePost method to work with GraphQL

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,24 +14,43 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    const graphqlQuery = {
+      query: `
+        {
+          singlePost(postId: "${postId}") {
+            title
+            imageUrl
+            content
+            creator {
+              name
+            }
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    }
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
-        Authorization: 'Bearer ' + this.props.token
-      }
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch status');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+          throw new Error('Fetching post failed!');
+        }
         this.setState({
-          title: resData.post.title,
-          author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
-          date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
+          title: resData.data.singlePost.title,
+          author: resData.data.singlePost.creator.name,
+          image: 'http://localhost:8080/' + resData.data.singlePost.imageUrl,
+          date: new Date(resData.data.singlePost.createdAt).toLocaleDateString('en-US'),
+          content: resData.data.singlePost.content
         });
       })
       .catch(err => {
