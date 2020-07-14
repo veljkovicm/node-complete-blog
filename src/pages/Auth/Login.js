fa462402724eb6de98cc89fbbe1af4c956d7d67b
Milan <comiv92@gmail.com>
2020-07-14 13:30:25 +0200

    Updates the login flow to use GraphQL

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -18,7 +18,7 @@ class Login extends Component {
         value: '',
         valid: false,
         touched: false,
-        validators: [required, length({ min: 5 })]
+        validators: [required, length({ min: 2 })]
       },
       formIsValid: false
     }
