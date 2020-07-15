1eefb6c1b6fa3904446b4540bd341bc31b682763
Milan <comiv92@gmail.com>
2020-07-15 16:40:19 +0200

    Adds the updatePost query

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -184,6 +184,29 @@ class Feed extends Component {
         `
       }
   
+      if (this.state.editPost) {
+        graphqlQuery = {
+          query: `
+            mutation {
+              updatePost(id: "${this.state.editPost._id}", postInput: {
+                title: "${postData.title}",
+                content: "${postData.content}",
+                imageUrl: "${imageUrl}"
+              }) {
+                _id
+                title
+                content
+                imageUrl
+                creator {
+                  name
+                }
+                createdAt
+              }
+            }
+          `
+        }
+      }
+
       return fetch('http://localhost:8080/graphql', {
         method: 'POST',
         body: JSON.stringify(graphqlQuery),
@@ -206,14 +229,16 @@ class Feed extends Component {
         throw new Error('Post creation failed!');
       }
 
-      console.log('resData', resData);
+      
+      let resDataField = this.state.editPost ? 'updatePost' : 'createPost';
+      console.log('resData.data', resData.data);
       const post = {
-        _id: resData.data.createPost._id,
-        title: resData.data.createPost.title,
-        content: resData.data.createPost.content,
-        creator: resData.data.createPost.creator,
-        createdAt: resData.data.createPost.createdAt,
-        imagePath: resData.data.createPost.imageUrl
+        _id: resData.data[resDataField]._id,
+        title: resData.data[resDataField].title,
+        content: resData.data[resDataField].content,
+        creator: resData.data[resDataField].creator,
+        createdAt: resData.data[resDataField].createdAt,
+        imagePath: resData.data[resDataField].imageUrl
       };
       this.setState(prevState => {
         let updatedPosts = [...prevState.posts];
